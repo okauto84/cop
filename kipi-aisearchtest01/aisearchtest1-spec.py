b7300279f7b47f2fef19af8d7fb3d219903a3be2
okauto84 <okauto84@gmail.com>
2026-03-10 12:15:24 +0900

    ch

--- kipi-aisearchtest01/aisearchtest1-spec.py
+++ kipi-aisearchtest01/aisearchtest1-spec.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
-
 import streamlit as st
 import pandas as pd
 
+st.title("검색 테스트")
 
-안녕하세요. 
+tab1, tab2 = st.tabs(["AI분석", "공보"])
+
+with tab1:
+    st.header("AI분석")
+    st.write("AI분석 탭 내용을 여기에 작성하세요.")
+
+with tab2:
+    st.header("공보")
+    st.write("공보 탭 내용을 여기에 작성하세요.") 
